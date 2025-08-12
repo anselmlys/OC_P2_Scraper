2fee30a92d023bf13a6569efc004ef99a1eb9c8f
anselmlys <anselm.lisa@gmail.com>
2025-08-12 11:37:15 +0200

    Ajout des exceptions liées à la sauvegarde de donnée

--- src/load.py
+++ src/load.py
@@ -3,9 +3,12 @@ import csv
 
 
 def save_csv(dictionary):
-    Path("data").mkdir(parents=True, exist_ok=True)
+    try:
+        Path("data").mkdir(parents=True, exist_ok=True)
 
-    with open('data/book_information.csv', 'w', newline='', encoding='utf-8') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=dictionary.keys())
-        writer.writeheader()
-        writer.writerow(dictionary)
+        with open('data/book_information.csv', 'w', newline='', encoding='utf-8') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=dictionary.keys())
+            writer.writeheader()
+            writer.writerow(dictionary)
+    except PermissionError as errp:
+        print("Permission Error", {errp})
